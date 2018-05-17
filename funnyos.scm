91940b6fe0d8b6efda4dc8524794c58e0acbf271
fangp <fangp@wangsu.com>
2018-05-17 18:14:35 +0800

    funnyos.scm

--- funnyos.scm
+++ funnyos.scm
@@ -22,3 +22,44 @@
 (define-generic move-resource)
 (define-method (move-resource (s "Register"@00) (t "Register"@00)) (...))
 
+
+(define (make-JobQueue self priority quantum the-time??)
+	(lambda (message)
+		(case message
+			((*fork*)
+				(lambda ()
+					()))
+			(else ()))))
+
+(define (make-Job id processes arrive-time turnaround-time wait-time response-time last-ran finished) 
+	(lambda (message)
+		(case message
+			((*init-stat*)
+				(lambda ()
+					(begin (set! turnaround-time 0)
+						(set! wait-time 0)
+						(set! response-time -1))))
+			((*set-response-time*)
+				(lambda (the-time)
+					(if (eqv response-time -1) (set! response-time the-time))))
+			((*calc-wait-time*)
+				(lambda (the-time)
+					(set! wait-time (+ wait-time (- the-time last-ran)))))
+			(else ()))))
+
+(define (make-MultiLevelFeedbackQueue )
+	())
+
+(define round-robin (lambda (job-queue)
+	(if (is-not-empty? job-queue) 
+		(let* ((job (dequeue job-queue)) ()) 
+			(ask job *set-response-time*)
+			(ask job *calc-wait-time*)
+			()))))
+
+;
+
+; https://github.com/shreyakamath2311/Xv6-MLFQ-Scheduler/tree/master/xv6/kernel
+
+
+
